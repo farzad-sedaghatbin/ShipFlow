0c4d79ef0139eb5e725422e73fd675c98e7c31cc
farzad <farzad@kixy.com>
2026-02-02 23:49:10 +0000

    refactor: update AuditRevisionEntity to implement Serializable and define custom revision fields

--- backend/src/main/java/com/github/farzadsedaghatbin/shipflow/entity/audit/AuditRevisionEntity.java
+++ backend/src/main/java/com/github/farzadsedaghatbin/shipflow/entity/audit/AuditRevisionEntity.java
@@ -3,19 +3,32 @@ package com.github.farzadsedaghatbin.shipflow.entity.audit;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.envers.DefaultRevisionEntity;
 import org.hibernate.envers.RevisionEntity;
+import org.hibernate.envers.RevisionNumber;
+import org.hibernate.envers.RevisionTimestamp;
+
+import java.io.Serializable;
 
 /**
- * Custom revision entity that extends the default Envers revision
- * to include the username of who made the change.
+ * Custom revision entity that tracks audit information.
+ * Uses 'rev' column name to match the existing database schema.
  */
 @Entity
 @Table(name = "revinfo")
 @RevisionEntity(AuditRevisionListener.class)
 @Getter
 @Setter
-public class AuditRevisionEntity extends DefaultRevisionEntity {
+public class AuditRevisionEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @RevisionNumber
+    @Column(name = "rev")
+    private int id;
+
+    @RevisionTimestamp
+    @Column(name = "revtstmp")
+    private long timestamp;
 
     /**
      * The username of the user who made the change.
